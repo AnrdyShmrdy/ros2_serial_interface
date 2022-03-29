df6bd7940df681a81b52813e5e8ce8eff7467622
Andy Ponce <andy.ponce@outlook.com>
2022-03-29 14:03:14 -0400

    added more parameters and a launch file

--- ros2_serial_interface/serial_controller.py
+++ ros2_serial_interface/serial_controller.py
@@ -1,73 +1,81 @@
 # Node to publish a string topic
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
 import serial
-#TODO: Create a launch file for this Node
+
+# Handle Twist messages, linear and angular velocity
+from geometry_msgs.msg import Twist
 class SerialController(Node):
 	def __init__(self):
 		super().__init__('serial_controller')
-		self.declare_parameter('device', '/dev/ttyACM0') #TODO: Define and set this in a .yaml config file + launch file
-		self.declare_parameter('topic', 'topic') #TODO: Define and set this in a .yaml config file + launch file
-		#Example of Potential robot command: self.declare_paramter('turnLeft', 'leftMotorRoutine') 
-		
-  		#TODO: Change below line to fetch the parameter value from a namespace in a .yaml config file
-		self.ser = serial.Serial(self.get_parameter('device').get_parameter_value().string_value,
+		#TODO: Try and see if it's possible to use parameters without declaration of default value
+		#Default Value declarations of ros2 params:
+		self.declare_parameter('device', '/dev/ttyACM0')
+		self.declare_parameter('wheel_instructions_topic', 'wheel_instructions_topic')
+		self.declare_parameter('move_forward_lin_vel', 1.0)
+		self.declare_parameter('move_backward_lin_vel', -1.0)
+		self.declare_parameter('turn_left_ang_vel', 1.0)
+		self.declare_parameter('turn_right_ang_vel', -1.0)
+		self.declare_parameter('move_forward_val', 1)
+		self.declare_parameter('move_backward_val', 2)
+		self.declare_parameter('turn_right_val', 3)
+		self.declare_parameter('turn_left_val', 4)
+		self.wheel_topic_name = self.get_parameter('wheel_instructions_topic').get_parameter_value().string_value
+		self.device_name = self.get_parameter('device').get_parameter_value().string_value
+		self.forward_vel = float(self.get_parameter('move_forward_lin_vel').get_parameter_value().double_value)
+		self.backward_vel = float(self.get_parameter('move_backward_lin_vel').get_parameter_value().double_value)
+		self.turn_right_vel = float(self.get_parameter('turn_right_ang_vel').get_parameter_value().double_value)
+		self.turn_left_vel = float(self.get_parameter('turn_left_ang_vel').get_parameter_value().double_value)
+		self.move_forward_val = bin(self.get_parameter('move_forward_val').get_parameter_value().integer_value)
+		self.move_backward_val = bin(self.get_parameter('move_backward_val').get_parameter_value().integer_value)
+		self.turn_right_val = bin(self.get_parameter('turn_right_val').get_parameter_value().integer_value)
+		self.turn_left_val = bin(self.get_parameter('turn_left_val').get_parameter_value().integer_value)
+		print(self.device_name)
+		print(self.wheel_topic_name)
+		print(self.forward_vel)
+		print(self.backward_vel)
+		print(self.turn_left_vel)
+		print(self.turn_right_vel)
+		print(self.move_forward_val)
+		print(self.move_backward_val)
+		print(self.turn_left_val)
+		print(self.turn_right_val)
+		self.ser = serial.Serial(self.device_name,
                            9600, #Note: Baud Rate must be the same in the arduino program, otherwise signal is not recieved!
                            timeout=4)
 		
-  		#TODO: Change below subscription to fetch the parameter value from a namespace in a .yaml config file
-		self.subscriber = self.create_subscription(String, 
-                                              self.get_parameter('topic').get_parameter_value().string_value, 
+		self.subscriber = self.create_subscription(Twist, 
+                                              self.wheel_topic_name, 
                                               self.serial_listener_callback, 
                                               10)
 		self.subscriber # prevent unused variable warning
 		self.ser.reset_input_buffer()
 	
 	def serial_listener_callback(self, msg):
-		#TODO: Change this so that commands are derived from a variable array of paramters. 
-  		#TODO: Make it so that we can avoid editing this function in the future by editing the paramater values
-		if msg.data == "all_on": #TODO: future format ideally would be "if msg.data == command_paramater"
-			self.ser.write(b'9') #TODO: future format ideally would be "if msg.data == command_value"
+		"""Move Forward"""
+		if msg.linear.x == self.forward_vel:
+			self.ser.write(self.move_forward_val)
 			line = self.ser.readline().decode('utf-8').rstrip()
 			print(line) #TODO: "command_name" sent
-		if msg.data == "all_off": #TODO: future format ideally would be "if msg.data == command_value"
-			self.ser.write(b'0')  #TODO: future format ideally would be "if msg.data == command_value"
+			print("move-forward: " + str(self.move_forward_val))
+		"""Move Backward"""
+		if msg.linear.x == self.backward_vel:
+			self.ser.write(self.move_backward_val)
 			line = self.ser.readline().decode('utf-8').rstrip()
 			print(line)  #TODO: "command_name" sent
-		if msg.data == "led1_on":
-			self.ser.write(b'1') #TODO: future format ideally would be "if msg.data == command_value"
+			print("move-backward: " + str(self.move_backward_val))
+		"""Turn Left"""
+		if msg.angular.z == self.turn_left_vel:
+			self.ser.write(self.turn_left_val)
 			line = self.ser.readline().decode('utf-8').rstrip()
 			print(line)  #TODO: "command_name" sent
-		if msg.data == "led2_on":
-			self.ser.write(b'2') #TODO: future format ideally would be "if msg.data == command_value"
+			print("turn-left: " + str(self.turn_left_val))
+		"""Turn Right"""
+		if msg.angular.z == self.turn_right_vel:
+			self.ser.write(self.turn_right_val) #TODO: future format ideally would be "if msg.data == command_value"
 			line = self.ser.readline().decode('utf-8').rstrip()
 			print(line)  #TODO: "command_name" sent
-		if msg.data == "led3_on":
-			self.ser.write(b'3') #TODO: future format ideally would be "if msg.data == command_value"
-			line = self.ser.readline().decode('utf-8').rstrip()
-			print(line)  #TODO: "command_name" sent
-		if msg.data == "led4_on":
-			self.ser.write(b'4') #TODO: future format ideally would be "if msg.data == command_value"
-			line = self.ser.readline().decode('utf-8').rstrip()
-			print(line)  #TODO: "command_name" sent
-		if msg.data == "led1_off":
-			self.ser.write(b'5') #TODO: future format ideally would be "if msg.data == command_value"
-			line = self.ser.readline().decode('utf-8').rstrip()
-			print(line)  #TODO: "command_name" sent
-		if msg.data == "led2_off":
-			self.ser.write(b'6') #TODO: future format ideally would be "if msg.data == command_value"
-			line = self.ser.readline().decode('utf-8').rstrip()
-			print(line)  #TODO: "command_name" sent
-		if msg.data == "led3_off":
-			self.ser.write(b'7') #TODO: future format ideally would be "if msg.data == command_value"
-			line = self.ser.readline().decode('utf-8').rstrip()
-			print(line)  #TODO: "command_name" sent
-		if msg.data == "led4_off":
-			self.ser.write(b'8') #TODO: future format ideally would be "if msg.data == command_value"
-			line = self.ser.readline().decode('utf-8').rstrip()
-			print(line)  #TODO: "command_name" sent
-
+			print("turn-right: " + str(self.turn_right_val))
 
 def main(args=None):
 	rclpy.init(args=args)
--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'ros2_serial_interface'
 
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*_launch.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
